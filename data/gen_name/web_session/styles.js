1b152b669a1044df513e4d2c23c80d5b924d6f71
Matthew Wilpula <mattwilpula@gmail.com>
2025-05-01 19:21:40 -0400

    Pushing embeddable cytoscape graphs

---
+++ data/gen_name/web_session/styles.js
@@ -0,0 +1,178 @@
+var styles = [ {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.10.3",
+  "target_cytoscapejs_version" : "~2.1",
+  "title" : "default",
+  "style" : [ {
+    "selector" : "node",
+    "css" : {
+      "border-color" : "rgb(37,37,37)",
+      "width" : 60.0,
+      "font-size" : 12,
+      "text-valign" : "center",
+      "text-halign" : "center",
+      "shape" : "roundrectangle",
+      "font-family" : "SansSerif.plain",
+      "font-weight" : "normal",
+      "height" : 60.0,
+      "text-opacity" : 1.0,
+      "background-color" : "rgb(159,161,159)",
+      "border-opacity" : 1.0,
+      "color" : "rgb(0,0,0)",
+      "background-opacity" : 1.0,
+      "border-width" : 5.0,
+      "content" : "data(name)"
+    }
+  }, {
+    "selector" : "node[category = 'Water']",
+    "css" : {
+      "background-color" : "rgb(41,128,239)"
+    }
+  }, {
+    "selector" : "node[category = 'Fighting']",
+    "css" : {
+      "background-color" : "rgb(255,128,0)"
+    }
+  }, {
+    "selector" : "node[category = 'Rock']",
+    "css" : {
+      "background-color" : "rgb(175,169,129)"
+    }
+  }, {
+    "selector" : "node[category = 'Ground']",
+    "css" : {
+      "background-color" : "rgb(145,81,33)"
+    }
+  }, {
+    "selector" : "node[category = 'Electric']",
+    "css" : {
+      "background-color" : "rgb(250,192,0)"
+    }
+  }, {
+    "selector" : "node[category = 'Ice']",
+    "css" : {
+      "background-color" : "rgb(61,206,243)"
+    }
+  }, {
+    "selector" : "node[category = 'Grass']",
+    "css" : {
+      "background-color" : "rgb(63,161,41)"
+    }
+  }, {
+    "selector" : "node[category = 'Normal']",
+    "css" : {
+      "background-color" : "rgb(159,161,159)"
+    }
+  }, {
+    "selector" : "node[category = 'Steel']",
+    "css" : {
+      "background-color" : "rgb(96,161,184)"
+    }
+  }, {
+    "selector" : "node[category = 'Dragon']",
+    "css" : {
+      "background-color" : "rgb(80,96,225)"
+    }
+  }, {
+    "selector" : "node[category = 'Flying']",
+    "css" : {
+      "background-color" : "rgb(129,185,239)"
+    }
+  }, {
+    "selector" : "node[category = 'Psychic']",
+    "css" : {
+      "background-color" : "rgb(239,65,121)"
+    }
+  }, {
+    "selector" : "node[category = 'Bug']",
+    "css" : {
+      "background-color" : "rgb(145,161,25)"
+    }
+  }, {
+    "selector" : "node[category = 'Ghost']",
+    "css" : {
+      "background-color" : "rgb(112,65,112)"
+    }
+  }, {
+    "selector" : "node[category = 'Dark']",
+    "css" : {
+      "background-color" : "rgb(98,77,78)"
+    }
+  }, {
+    "selector" : "node[category = 'Fire']",
+    "css" : {
+      "background-color" : "rgb(230,40,41)"
+    }
+  }, {
+    "selector" : "node[category = 'Poison']",
+    "css" : {
+      "background-color" : "rgb(145,65,203)"
+    }
+  }, {
+    "selector" : "node[category = 'Fairy']",
+    "css" : {
+      "background-color" : "rgb(239,112,239)"
+    }
+  }, {
+    "selector" : "node[type = 'special']",
+    "css" : {
+      "width" : 60.0,
+      "height" : 60.0
+    }
+  }, {
+    "selector" : "node[type = 'physical']",
+    "css" : {
+      "width" : 60.0,
+      "height" : 60.0
+    }
+  }, {
+    "selector" : "node[type = 'status']",
+    "css" : {
+      "width" : 60.0,
+      "height" : 60.0
+    }
+  }, {
+    "selector" : "node[type = 'special']",
+    "css" : {
+      "shape" : "diamond"
+    }
+  }, {
+    "selector" : "node[type = 'physical']",
+    "css" : {
+      "shape" : "rectangle"
+    }
+  }, {
+    "selector" : "node[type = 'status']",
+    "css" : {
+      "shape" : "ellipse"
+    }
+  }, {
+    "selector" : "node:selected",
+    "css" : {
+      "background-color" : "rgb(255,255,0)"
+    }
+  }, {
+    "selector" : "edge",
+    "css" : {
+      "line-style" : "solid",
+      "source-arrow-color" : "rgb(0,0,0)",
+      "font-size" : 10,
+      "target-arrow-shape" : "none",
+      "opacity" : 1.0,
+      "line-color" : "rgb(132,132,132)",
+      "text-opacity" : 1.0,
+      "font-family" : "Dialog.plain",
+      "font-weight" : "normal",
+      "color" : "rgb(0,0,0)",
+      "width" : 2.0,
+      "source-arrow-shape" : "none",
+      "target-arrow-color" : "rgb(0,0,0)",
+      "content" : ""
+    }
+  }, {
+    "selector" : "edge:selected",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  } ]
+} ]
